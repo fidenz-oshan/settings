c959fed7780a460e51ed1062b4971cba2b9ad6c5
oshan wisumperuma <oshan.w@fidenz.com>
2016-10-19 18:34:56 +0530

    remove initializer

--- lib/settings/engine.rb
+++ lib/settings/engine.rb
@@ -1,9 +1,5 @@
 module Settings
   class Engine < ::Rails::Engine
-    initializer :append_migrations, before: :load_config_initializers do |_app|
-      config.paths['db/migrate'].expanded.each do |expanded_path|
-        Rails.application.config.paths['db/migrate'] << expanded_path
-      end
-    end
+    
   end
 end
