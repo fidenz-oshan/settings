5ad500628f75b39cbe70c6e12d86fa7cc5d803bb
oshan wisumperuma <oshan.w@fidenz.com>
2016-10-19 18:44:56 +0530

    fix foreign_key

--- db/migrate/20161019114557_create_settings.rb
+++ db/migrate/20161019114557_create_settings.rb
@@ -2,7 +2,7 @@ class CreateSettings < ActiveRecord::Migration
   def change
     create_table :settings do |t|
       t.timestamps null: false
-      t.belongs_to :settings_scopes, foreign_key: true
+      t.belongs_to :settings_scope, foreign_key: true
       t.integer :scope
       t.string :slug
       t.string :title
