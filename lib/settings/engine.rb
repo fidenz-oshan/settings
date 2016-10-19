cbae8694a7416580f9a7c56b716df385c6c9a24b
oshan wisumperuma <oshan.w@fidenz.com>
2016-10-19 19:00:15 +0530

    load ActiveAdmin resources

--- lib/settings/engine.rb
+++ lib/settings/engine.rb
@@ -1,5 +1,7 @@
 module Settings
   class Engine < ::Rails::Engine
-    
+    initializer 'main_app' do
+      ActiveAdmin.application.load_paths += Dir[File.dirname(__FILE__) + '../../app/admin']
+    end
   end
 end
