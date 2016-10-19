1f1f0bfd2db69258f98b09306d1542bc685aa011
oshan wisumperuma <oshan.w@fidenz.com>
2016-10-19 18:30:04 +0530

    update initalizer

--- lib/settings/engine.rb
+++ lib/settings/engine.rb
@@ -1,10 +1,8 @@
 module Settings
   class Engine < ::Rails::Engine
-    initializer :append_migrations do |app|
-      unless app.root.to_s.match root.to_s
-        config.paths['db/migrate'].expanded.each do |expanded_path|
-          app.config.paths['db/migrate'] << expanded_path
-        end
+    initializer :append_migrations, before: :load_config_initializers do |_app|
+      config.paths['db/migrate'].expanded.each do |expanded_path|
+        Rails.application.config.paths['db/migrate'] << expanded_path
       end
     end
   end
--- lib/settings/version.rb
+++ lib/settings/version.rb
@@ -1,3 +1,3 @@
 module Settings
-  VERSION = '1.0.2'.freeze
+  VERSION = '1.0.3'.freeze
 end
