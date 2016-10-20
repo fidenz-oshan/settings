729185106947c9e568950b948f5002db1d82e8d0
oshan wisumperuma <oshan.w@fidenz.com>
2016-10-20 13:57:25 +0530

    minor refactor

--- app/admin/setting.rb
+++ app/admin/setting.rb
@@ -6,22 +6,22 @@ ActiveAdmin.register Settings::Scope, as: 'Setting' do
 
   form do |f|
     f.inputs resource.title do
-      f.has_many :settings, new_record: false, heading: false, sortable: :order_id do |option|
-        case option.object.input_type
+      f.has_many :settings, new_record: false, heading: false, sortable: :order_id do |setting_f|
+        case setting_f.object.input_type
         when 'video'
-          option.input :value, label: option.object.title, as: :rich_picker, config: {
+          setting_f.input :value, label: setting_f.object.title, as: :rich_picker, config: {
             style: 'width: 400px !important;', type: 'video'
           }
         when 'input'
-          option.input :value, label: option.object.title
+          setting_f.input :value, label: setting_f.object.title
         when 'dropdown'
-          option.input :value, label: option.object.title, as: :select2, collection: option.object.input_values.split(',').map { |e| [e.to_s, e.to_s] }
+          setting_f.input :value, label: setting_f.object.title, as: :select2, collection: setting_f.object.input_values.split(',').map { |e| [e.to_s, e.to_s] }
         when 'radio'
-          option.input :value, label: option.object.title, as: :radio
+          setting_f.input :value, label: setting_f.object.title, as: :radio
         when 'checkbox'
-          option.input :value, label: option.object.title, as: :check_boxes, collection: option.object.input_values.split(',').map { |e| [e.to_s, e.to_s] }
+          setting_f.input :value, label: setting_f.object.title, as: :check_boxes, collection: setting_f.object.input_values.split(',').map { |e| [e.to_s, e.to_s] }
         end
-        option.semantic_errors
+        setting_f.semantic_errors
       end
     end
     f.actions
