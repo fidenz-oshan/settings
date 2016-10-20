94cb649c9340f0990850d4ef7851b36c8ac705c3
oshan wisumperuma <oshan.w@fidenz.com>
2016-10-20 09:52:21 +0530

    add autoloaders

--- lib/settings/engine.rb
+++ lib/settings/engine.rb
@@ -1,5 +1,21 @@
 module Settings
   class Engine < ::Rails::Engine
     isolate_namespace Settings
+
+    initializer :settings do
+      ActiveAdmin.application.load_paths += Dir[File.dirname(__FILE__) + '../../app/admin']
+    end
+
+    initializer 'settings', before: :load_config_initializers do |_app|
+      config.paths['db/migrate'].expanded.each do |expanded_path|
+        Rails.application.config.paths['db/migrate'] << expanded_path
+      end
+    end
+
+    initializer 'settings', after: :load_config_initializers do |_app|
+      Settings.load_files.each do |file|
+        require_relative File.join('../..', file)
+      end
+    end
   end
 end
